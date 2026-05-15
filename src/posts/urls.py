1e2858418026574ccc6333078e1cb5efa2a8d280
Artem Ruban <artemruban762@gmail.com>
2026-05-16 00:17:43 +0200

    add urls for post_list and post_detail

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='post_list'),
+    path('category/<slug:category_slug>/', views.post_list, name = 'post_list_by_categoty'),
+    path('<slug:slug>/', views.post_detail, name = 'post_detail'),
 ]
