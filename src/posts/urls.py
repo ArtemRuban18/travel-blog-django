8ae233c0602b12e79bad79002274e9c40b35805e
Artem Ruban <artemruban762@gmail.com>
2026-05-19 23:25:01 +0200

    I corrected issues and added post_list and post_detail html pages

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='post_list'),
-    path('category/<slug:category_slug>/', views.post_list, name = 'post_list_by_categoty'),
+    path('', views.post_list, name='post_list'),
+    path('category/<slug:category_slug>/', views.post_list, name = 'post_list_by_category'),
     path('<slug:slug>/', views.post_detail, name = 'post_detail'),
 ]
