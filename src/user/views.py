29be710729bc67e62f8c316b9af2cbc5c383c588
Artem Ruban <artemruban762@gmail.com>
2026-05-20 00:04:04 +0200

    A designed auth system for users and created html files(now they're empty)

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserLoginForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! Ypu can now log in')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('post_list')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form  = UserLoginForm()
+    return render(request, 'user/login.html', {'form': form})
