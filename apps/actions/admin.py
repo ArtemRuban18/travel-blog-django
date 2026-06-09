fa4909613f524b7c131b78cda24addaf2cb4c41a
Artem Ruban <artemruban762@gmail.com>
2026-06-09 22:19:39 +0200

    Delete likes from project

--- apps/actions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Likes
-
-@admin.register(Likes)
-class LikesAdmin(admin.ModelAdmin):
-    list_display = ['user', 'content_type', 'content_id']
-    list_filter = ['user','content_id']
-    search_fields = ['user']
--- apps/actions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'actions'
-
-urlpatterns = [
-    path('like-post/<slug:slug>/', views.like_post, name = "like_post"),
-    path('like-comment/<int:id>/', views.like_comment, name = "like_comment"),
-]
